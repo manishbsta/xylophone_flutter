c28eb874e3664098b3bc2e4ea646f8b56901e28c
Manish Bista <manishbsta@users.noreply.github.com>
2020-08-21 13:45:09 +0545

    Initial commit

---
+++ android/app/src/main/java/manishbista/com/xylophone_flutter/MainActivity.java
@@ -0,0 +1,6 @@
+package manishbista.com.xylophone_flutter;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Xylophone(),
+      ),
+    ),
+  );
+}
+
+class Xylophone extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+
+      ],
+    );
+  }
+}
+
